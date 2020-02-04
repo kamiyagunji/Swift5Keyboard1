56d38cbbcb2bcf8bb48dbf6236321801a64cf2df
kamiyagunji <kamiyagunji@icloud.com>
2020-02-04 10:19:56 +0900

    ソースコード追加

--- Swift5Keyboard1/ViewController.swift
+++ Swift5Keyboard1/ViewController.swift
@@ -8,13 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var logoImageView: UIImageView!
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var passWordTextField: UITextField!
+
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var passWordLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        userNameTextField.delegate = self
+        passWordTextField.delegate = self
+
     }
 
-
+    @IBAction func login(_ sender: Any) {
+        
+        logoImageView.image = UIImage(named: "loginOK")
+        
+        userNameLabel.text = userNameTextField.text
+        passWordLabel.text = passWordTextField.text
+        
+        view.endEditing(true)
+    }
+    
+    // タッチでキーボードを閉じる
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        view.endEditing(true)
+    }
+    
+    // リターンキーを押したときにキーボードを閉じる
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return textField.resignFirstResponder()
+    }
+    
 }
 
